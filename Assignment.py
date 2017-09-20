b516a4d3e8eee11db12579a59254c0660373eb3b
kromadown <yaminfarras41@gmail.com>
2017-09-20 08:27:28 +0700

    Third Commit v.2

--- Assignment.py
+++ Assignment.py
@@ -6,3 +6,14 @@ if list(var) == list(rev_var):
     print ("It's Palindrome")
 else:
     print ("It's not Palindrome")
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+result = [fib(n) for n in range (1,11)]
+print (result)
